522a122b669219f52b9cdf9857497e221e4ea414
Maks0123 <vadimcorporateukr@gmail.com>
2020-03-05 22:57:19 +0200

    It is just socket App

---
+++ tcpApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace tcpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tcp client started");
+            var message = "GET / HTTP/1.0\nHost: selin.in.ua\n\n";
+
+            try
+            {
+               var port = 80;
+               //var serverAddr = "0.tcp.ngrok.io";
+               var serverAddr = "127.0.0.1";
+               var client = new TcpClient(serverAddr, port);
+               var data = System.Text.Encoding.ASCII.GetBytes(message);
+               NetworkStream stream = client.GetStream();
+
+               stream.Write(data, 0, data.Length);
+
+               Console.WriteLine("Sent {0}", message);
+
+               var responseData = new byte[256];
+               int bytesRead = stream.Read(responseData, 0, responseData.Length);
+               var responseMessage = System.Text.Encoding.ASCII.GetString(responseData, 0, bytesRead);
+               Console.WriteLine("Received {0}", responseMessage);
+               stream.Close();
+               client.Close();
+
+            }
+            catch (System.IO.IOException e)
+        {
+            Console.WriteLine("Error reading from {0}. Message = {1}",  e.Message);
+        }
+        
+          
+           
+        }
+    }
+}
---
+++ tcpServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace tcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Kestrell");
+                   string IPaddress;
+            try
+            {
+              
+                var port = 80;
+                var localAddr = IPAddress.Parse("127.0.0.1");
+                var server = new TcpListener(localAddr, port);
+                server.Start();
+                var bytes = new byte[256];
+                while (true)
+                {
+                    Console.Write("waiting for connection");
+                    var client = server.AcceptTcpClient();
+                    Console.WriteLine("Accepted connection from {0}", client.Client.RemoteEndPoint);
+                    NetworkStream stream = client.GetStream();
+                    var bytesReadCount = stream.Read(bytes, 0, bytes.Length);
+                    var data = System.Text.Encoding.ASCII.GetString(bytes, 0, bytesReadCount);
+                    data = data.ToUpper();
+                    var response = System.Text.Encoding.ASCII.GetBytes(data);
+                    stream.Write(response, 0, response.Length);
+                    Console.WriteLine("sent: {0}", data);
+                    client.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception {0}", e);
+            }
+        }
+    }
+}
